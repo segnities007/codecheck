69e134ded690b78417c61e8f60b6c691099fcd83
seg <segnities007@gmail.com>
2024-12-02 20:42:08 +0900

    modify most part of issues2

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
@@ -3,12 +3,16 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
+import java.time.ZoneId
 import java.util.*
 
 class TopActivity : AppCompatActivity(R.layout.activity_top) {
 
     companion object {
-        lateinit var lastSearchDate: Date
+        var lastSearchDate: Date = Date()
     }
 }
